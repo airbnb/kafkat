7b0879de8fdfd821ccd1a1327b899b9db67f316f
Nelson Gauthier <nelson@airbnb.com>
2014-08-05 08:37:38 -0700

    Parallelize Zookeeper enumeration

--- lib/kafkat/interface/zookeeper.rb
+++ lib/kafkat/interface/zookeeper.rb
@@ -1,3 +1,5 @@
+require 'thread'
+
 module Kafkat
   module Interface
     class Zookeeper
@@ -12,13 +14,18 @@ module Kafkat
       def get_brokers(ids=nil)
         brokers = {}
         ids ||= zk.children(brokers_path)
-        ids.each do |id|
+
+        threads = ids.map do |id|
           id = id.to_i
-          begin
-            brokers[id] = get_broker(id)
-          rescue
+          Thread.new do
+            begin
+              brokers[id] = get_broker(id)
+            rescue
+            end
           end
         end
+        threads.map(&:join)
+
         brokers
       end
 
@@ -33,12 +40,17 @@ module Kafkat
       def get_topics(names=nil)
         topics = {}
         names ||= zk.children(topics_path)
-        names.each do |name|
-          begin
-            topics[name] = get_topic(name)
-          rescue => e
+
+        threads = names.map do |name|
+          Thread.new do
+            begin
+              topics[name] = get_topic(name)
+            rescue => e
+            end
           end
         end
+        threads.map(&:join)
+
         topics
       end
 
@@ -49,19 +61,22 @@ module Kafkat
 
         partitions = []
         path2 = topic_partitions_path(name)
-        zk.children(path2).each do |id|
+
+        threads = zk.children(path2).map do |id|
           id = id.to_i
+          Thread.new do
+            path3 = topic_partition_state_path(name, id)
+            partition_string = zk.get(path3).first
+            partition_json = JSON.parse(partition_string)
 
-          path3 = topic_partition_state_path(name, id)
-          partition_string = zk.get(path3).first
-          partition_json = JSON.parse(partition_string)
+            replicas = topic_json['partitions'][id.to_s]
+            leader = partition_json['leader']
+            isr = partition_json['isr']
 
-          replicas = topic_json['partitions'][id.to_s]
-          leader = partition_json['leader']
-          isr = partition_json['isr']
-
-          partitions << Partition.new(name, id, replicas, leader, isr)
+            partitions << Partition.new(name, id, replicas, leader, isr)
+          end
         end
+        threads.map(&:join)
 
         partitions.sort_by!(&:id)
         Topic.new(name, partitions)
@@ -76,7 +91,7 @@ module Kafkat
         partition_json['leader'] = broker_id
         new_string = JSON.dump(partition_json)
 
-        unless zk.set(path, new_string, :version => stat.version)
+        unless zk.set(path, new_string, version: stat.version)
           raise ChangedDuringUpdateError
         end
       end

c76cc1359c2fff950a730aaf608451089e35d02a
Luca Luo <jiaxiluo@umich.edu>
2015-06-22 12:00:43 -0700

    Update based on reivew
    1. Modify alignment of methods with multiple arguments; fit them in either
    one line or one per line.
    2. Reorganize code with strategy pattern to fully reuse code.
    3. Split smart assignment into smaller units under strategy pattern to
    achieve better readablity and testability.

--- lib/kafkat/command/reassign.rb
+++ lib/kafkat/command/reassign.rb
@@ -36,161 +36,135 @@ module Kafkat
         end
 
         if strategy == "smart"
-          assignments = generate_assignment_smart(topics, destination_broker_ids,
-                                                  replica_count, all_broker_ids)
+          assignment_strategy = SmartStrategy.new()
         elsif strategy == "load-balanced"
-          assignments = generate_assignment_load_balanced(topics, destination_broker_ids,
-                                                          replica_count)
+          assignment_strategy = LoadBalancedStrategy.new()
         else
           print "ERROR: Unrecognized strategy \"#{strategy}\".\n"
           exit 1
         end
 
+        broker_count = destination_broker_ids.size
+        assignments = []
+
+        topics.each do |_, topic|
+          topic_replica_count = get_topic_replica_count(topic, replica_count)
+          if topic_replica_count > broker_count
+            print "ERROR: Replication factor (#{topic_replica_count}) "\
+                  "is larger than brokers (#{broker_count}).\n"
+            exit 1
+          end
+
+          assignments += assignment_strategy.generate_topic_assignment(
+            topic, topic_replica_count, destination_broker_ids, replica_count, all_broker_ids)
+        end
+
         prompt_and_execute_assignments(assignments)
       end
+    end
 
+    # This is how Kafka's AdminUtils determines these values.
+    def get_topic_replica_count(topic, replica_count)
+      if topic.partitions.empty?
+        print "ERROR: Topic \"#{topic.name}\" has no partition.\n"
+        exit 1
+      end
+      replica_count || topic.partitions[0].replicas.size
+    end
+
+    class AssignmentStrategy
+      def generate_topic_assignment(
+        topic, topic_replica_count, destination_broker_ids, all_broker_ids, broker_count)
+
+        raise NotImplementedError.new()
+      end
+    end
+
+    class LoadBalancedStrategy < AssignmentStrategy
+      # *** This logic is duplicated from Kakfa 0.8.1.1 ***
+      def generate_topic_assignment(
+        topic, topic_replica_count, destination_broker_ids, _, broker_count)
+
+        assignment = []
+        start_index = Random.rand(broker_count)
+        replica_shift = Random.rand(broker_count)
+
+        topic.partitions.each do |p|
+          replica_shift += 1 if p.id > 0 && p.id % broker_count == 0
+          first_replica_index = (p.id + start_index) % broker_count
+
+          replicas = [destination_broker_ids[first_replica_index]]
+
+          (0...topic_replica_count-1).each do |i|
+            shift = 1 + (replica_shift + i) % (broker_count - 1)
+            index = (first_replica_index + shift) % broker_count
+            replicas << destination_broker_ids[index]
+          end
+
+          replicas.reverse!
+          assignment << Assignment.new(topic.name, p.id, replicas)
+        end
+
+        assignment
+      end
+    end
+
+    class SmartStrategy < AssignmentStrategy
       # This is the default assignment generation strategy.
       # There are two requirements:
       #   1. replicas should be evenly distributed among brokers;
       #   2. replicas moving around should be avoided as possible.
       # We also need to address that user can sepcify both number of brokers and
       # replication factors.
-      #
-      # To address the requirements efficiently, two passes are used to determine assignemnt.
-      # In the first pass, existing assignments where partitions with too many replicas (probably
-      # due to decrese of replication factor) and brokers with too many replicas (probably due to
-      # decrease of replication factor or increase of number of brokers) are removed.
-      # Brokers not in destination brokers are also removed from assignment in the first pass.
-      #
-      # In the second pass, partitions are assigned with available brokers if neither the partition
-      # nor the brokers are full. Assignemnts where brokers are full are swaped with availble
-      # brokers.
-      #
-      # To facilitate the two passes, two hash tables are maintained to keep track of quota
-      # on each broker and assigned partitions on each broker.
-      def generate_assignment_smart(topics, destination_broker_ids, replica_count, all_broker_ids)
-        broker_count = destination_broker_ids.size
-        assignments = []
+      def generate_topic_assignment(
+        topic, topic_replica_count, destination_broker_ids, all_broker_ids, _)
 
-        topics.each do |_, t|
-          partition_count = t.partitions.size
-          topic_replica_count = get_topic_replica_count(t, replica_count)
+        assignment = []
 
-          if topic_replica_count > broker_count
-            print "ERROR: Replication factor (#{topic_replica_count}) "\
-                  "is larger than brokers (#{broker_count}).\n"
-            exit 1
-          end
+        partition_count = topic.partitions.size
+        num_partitions_on_broker = build_num_partitions_on_broker_map(topic, all_broker_ids)
+        partitions_quota_on_broker = generate_partitions_quota_on_broker(
+          partition_count, topic_replica_count, destination_broker_ids, num_partitions_on_broker)
 
-          num_partitions_on_broker = build_num_partitions_on_broker_map(t, all_broker_ids)
-          partitions_quota_on_broker =
-            generate_partitions_quota_on_broker(partition_count, topic_replica_count,
-                                                destination_broker_ids, num_partitions_on_broker)
+        remove_replicas_with_overfilled_broker_and_partition(
+          topic,
+          topic_replica_count,
+          num_partitions_on_broker,
+          partitions_quota_on_broker,
+          destination_broker_ids)
 
+        topic.partitions.each do |p|
+          available_brokers = find_available_brokers(
+            destination_broker_ids, p, num_partitions_on_broker, partitions_quota_on_broker)
 
+          remove_assignment_with_overfilled_broker(
+            p,
+            available_brokers,
+            topic_replica_count,
+            num_partitions_on_broker,
+            partitions_quota_on_broker)
 
-          # First pass: remove assignment where both broker and partition exceed quota,
-          # or brokers not in destination brokers.
-          t.partitions.each do |p|
-            p.replicas.delete_if do |broker|
-              if (p.replicas.size > topic_replica_count &&
-                  num_partitions_on_broker[broker] > partitions_quota_on_broker[broker]) ||
-                 !destination_broker_ids.include?(broker)
+          fill_underfilled_partition(
+            p, topic_replica_count, available_brokers, num_partitions_on_broker)
+          trim_overfilled_partition(p, topic_replica_count, num_partitions_on_broker)
 
-                num_partitions_on_broker[broker] -= 1
-                true
-              end
-            end
-          end
-
-          # Second pass
-          t.partitions.each do |p|
-            # find brokers not assigned with the partition and not meet quota
-            available_brokers = destination_broker_ids - p.replicas
-            available_brokers.delete_if do |broker|
-              num_partitions_on_broker[broker] >= partitions_quota_on_broker[broker]
-            end
-
-            # remove broker exceeding quota if available brokers exit
-            p.replicas.delete_if do |broker|
-              if p.replicas.size + available_brokers.size > topic_replica_count &&
-                num_partitions_on_broker[broker] > partitions_quota_on_broker[broker]
-
-                num_partitions_on_broker[broker] -= 1
-                true
-              end
-            end
-
-            # fill empty replicas or remove extra replicas
-            while p.replicas.size < topic_replica_count
-              assigned_broker = available_brokers.shift
-              num_partitions_on_broker[assigned_broker] += 1
-              p.replicas << assigned_broker
-            end
-            while p.replicas.size > topic_replica_count
-              removed_broker = p.replicas.shift
-              num_partitions_on_broker[removed_broker] -= 1
-            end
-
-            assignments << Assignment.new(t.name, p.id, p.replicas)
-          end
+          assignment << Assignment.new(topic.name, p.id, p.replicas)
         end
 
-        assignments
+        assignment
       end
 
-      # *** This logic is duplicated from Kakfa 0.8.1.1 ***
-      def generate_assignment_load_balanced(topics, destination_broker_ids, replica_count)
-        broker_count = destination_broker_ids.size
-        assignments = []
-
-        topics.each do |_, t|
-          topic_replica_count = get_topic_replica_count(t, replica_count)
-
-          if topic_replica_count > broker_count
-            print "ERROR: Replication factor (#{topic_replica_count}) "\
-                  "is larger than brokers (#{broker_count}).\n"
-            exit 1
-          end
-
-          start_index = Random.rand(broker_count)
-          replica_shift = Random.rand(broker_count)
-
-          t.partitions.each do |p|
-            replica_shift += 1 if p.id > 0 && p.id % broker_count == 0
-            first_replica_index = (p.id + start_index) % broker_count
-
-            replicas = [destination_broker_ids[first_replica_index]]
-
-            (0...topic_replica_count-1).each do |i|
-              shift = 1 + (replica_shift + i) % (broker_count - 1)
-              index = (first_replica_index + shift) % broker_count
-              replicas << destination_broker_ids[index]
-            end
-
-            replicas.reverse!
-            assignments << Assignment.new(t.name, p.id, replicas)
-          end
-        end
-
-        assignments
-      end
-
-      # This is how Kafka's AdminUtils determines these values.
-      def get_topic_replica_count(topic, replica_count)
-        if topic.partitions.empty?
-          print "ERROR: Topic \"#{topic.name}\" has no partition.\n"
-          exit 1
-        end
-        replica_count || topic.partitions[0].replicas.size
-      end
-
+    private
       # Assign quota to brokers.
       # Quota is evenly distributed among detination brokers where as those destination brokers
       # with more replicas initially might be assigned with one more if quota cannot be
       # distrbuted completely evenly.
-      def generate_partitions_quota_on_broker(partition_count, topic_replica_count,
-                                              destination_broker_ids, num_partitions_on_broker)
+      def generate_partitions_quota_on_broker(
+        partition_count,
+        topic_replica_count,
+        destination_broker_ids,
+        num_partitions_on_broker)
+
         total_partitions = partition_count * topic_replica_count
         min_replicas_per_broker = total_partitions / destination_broker_ids.size
         remaining_replicas_quota = total_partitions % destination_broker_ids.size
@@ -206,6 +180,78 @@ module Kafkat
         end
 
         partitions_quota_on_broker
+      end
+
+      # Existing assignments where partitions with too many replicas ( due to decrese of
+      # replication factor) and brokers with too many replicas (due to decrease of replication
+      # factor or increase of number of brokers) are removed.
+      # Brokers not in destination brokers are also removed.
+      def remove_replicas_with_overfilled_broker_and_partition(
+        topic,
+        topic_replica_count,
+        num_partitions_on_broker,
+        partitions_quota_on_broker,
+        destination_broker_ids)
+
+        topic.partitions.each do |p|
+          p.replicas.delete_if do |broker|
+            if (p.replicas.size > topic_replica_count &&
+                num_partitions_on_broker[broker] > partitions_quota_on_broker[broker]) ||
+               !destination_broker_ids.include?(broker)
+
+              num_partitions_on_broker[broker] -= 1
+              true
+            end
+          end
+        end
+      end
+
+      # Find brokers not assigned with the partition and not meet quota
+      def find_available_brokers(
+        destination_broker_ids, partition, num_partitions_on_broker, partitions_quota_on_broker)
+
+        available_brokers = destination_broker_ids - partition.replicas
+        available_brokers.delete_if do |broker|
+          num_partitions_on_broker[broker] >= partitions_quota_on_broker[broker]
+        end
+        available_brokers
+      end
+
+      # Remove broker exceeding quota if enough available brokers exit.
+      # If not enough brokers available while existing broker exceeding quota, this means the broker
+      # is overfilled with other partitions, and will meet quota in later iteration.
+      def remove_assignment_with_overfilled_broker(
+        partition,
+        available_brokers,
+        topic_replica_count,
+        num_partitions_on_broker,
+        partitions_quota_on_broker)
+
+        partition.replicas.delete_if do |broker|
+          if partition.replicas.size + available_brokers.size > topic_replica_count &&
+            num_partitions_on_broker[broker] > partitions_quota_on_broker[broker]
+
+            num_partitions_on_broker[broker] -= 1
+            true
+          end
+        end
+      end
+
+      def fill_underfilled_partition(
+        partition, topic_replica_count, available_brokers, num_partitions_on_broker)
+
+        while partition.replicas.size < topic_replica_count
+          assigned_broker = available_brokers.shift
+          num_partitions_on_broker[assigned_broker] += 1
+          partition.replicas << assigned_broker
+        end
+      end
+
+      def trim_overfilled_partition(partition, topic_replica_count, num_partitions_on_broker)
+        while partition.replicas.size > topic_replica_count
+          removed_broker = partition.replicas.shift
+          num_partitions_on_broker[removed_broker] -= 1
+        end
       end
     end
   end
--- spec/lib/kafkat/command/reassign_spec.rb
+++ spec/lib/kafkat/command/reassign_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 
 module Kafkat
   RSpec.describe Command::Reassign do
-    let(:reassign) { Command::Reassign.new({}) }
+    let(:load_balanced_strategy) { Command::LoadBalancedStrategy.new() }
+    let(:smart_strategy) { Command::SmartStrategy.new() }
     let(:skewed_topic) { FactoryGirl.build(:skewed_topic) }
     let(:topic_not_distributed_evenly) { FactoryGirl.build(:topic_not_distributed_evenly) }
     let(:topic_rep_factor_one) { FactoryGirl.build(:topic_rep_factor_one) }
@@ -20,10 +21,12 @@ module Kafkat
 
     context 'strategy load-balanced' do
       it 'should evenly distribute replicas randomly' do
-        assignments = reassign.generate_assignment_load_balanced(
-          {"topic_name" => topic_rep_factor_two},
+        assignments = load_balanced_strategy.generate_topic_assignment(
+          topic_rep_factor_two,
+          2,
           [1, 2],
-          2)
+          [0, 1, 2],
+          3)
         expect(assignments).to have_exactly(5).Partitions
         assignments.each do |a|
           expect(a).to have_exactly(2).replicas
@@ -33,11 +36,12 @@ module Kafkat
 
     context 'default strategy smart' do
       it 'should evenly distribute replicas and minimize data movement' do
-        assignments = reassign.generate_assignment_smart(
-          {"topic_name" => topic_rep_factor_one},
-          [1, 2],
+        assignments = smart_strategy.generate_topic_assignment(
+          topic_rep_factor_one,
           1,
-          [0, 1, 2])
+          [1, 2],
+          [0, 1, 2],
+          3)
         expect(assignments).to have_exactly(5).Partitions
         expect([[1], [2]]).to include(assignments[0].replicas)
         expect(assignments[1].replicas).to eq([1])
@@ -47,11 +51,12 @@ module Kafkat
       end
 
       it 'should handle unevenly distribution properly' do
-        assignments = reassign.generate_assignment_smart(
-          {"topic_name" => topic_not_distributed_evenly},
-          [1, 2],
+        assignments = smart_strategy.generate_topic_assignment(
+          topic_not_distributed_evenly,
           1,
-          [0, 1, 2])
+          [1, 2],
+          [0, 1, 2],
+          3)
         expect(assignments).to have_exactly(5).Partitions
         expect(assignments[0].replicas).to eq([2])
         expect(assignments[1].replicas).to eq([2])
@@ -61,11 +66,12 @@ module Kafkat
       end
 
       it 'should handle more replications and brokers properly' do
-        assignments = reassign.generate_assignment_smart(
-          {"topic_name" => topic_rep_factor_one},
-          [0, 1, 2, 3],
+        assignments = smart_strategy.generate_topic_assignment(
+          topic_rep_factor_one,
           2,
-          [0, 1, 2, 3])
+          [0, 1, 2, 3],
+          [0, 1, 2, 3],
+          4)
         expect(assignments).to have_exactly(5).Partitions
         expect(assignments[0].replicas).to include(0)
         expect(assignments[1].replicas).to include(1)
@@ -78,11 +84,12 @@ module Kafkat
       end
 
       it 'should handle less replication and brokers properly' do
-        assignments = reassign.generate_assignment_smart(
-          {"topic_name" => topic_rep_factor_two},
-          [1, 2],
+        assignments = smart_strategy.generate_topic_assignment(
+          topic_rep_factor_two,
           1,
-          [0, 1, 2])
+          [1, 2],
+          [0, 1, 2],
+          3)
         expect(assignments).to have_exactly(5).Partitions
         expect(assignments[0].replicas).to eq([1])
         expect(assignments[1].replicas).to eq([2])
@@ -92,52 +99,15 @@ module Kafkat
       end
 
       it 'should handle skewed topic properly' do
-        assignments = reassign.generate_assignment_smart(
-          {"topic_name" => skewed_topic},
-          [0, 1],
+        assignments = smart_strategy.generate_topic_assignment(
+          skewed_topic,
           1,
-          [0, 1])
+          [0, 1],
+          [0, 1],
+          2)
         expect(assignments).to have_exactly(2).Partitions
         counts = count(assignments)
         expect(counts).to eq({0 => 1, 1 =>1})
-      end
-    end
-
-    context 'replication factor larger than destination broker size' do
-      it 'should fail' do
-        expect do
-          assignments = reassign.generate_assignment_load_balanced(
-            {"topic_name" => topic_rep_factor_two},
-            [1, 2],
-            3)
-        end.to raise_error(SystemExit)
-
-        expect do
-          assignments = reassign.generate_assignment_smart(
-            {"topic_name" => topic_rep_factor_two},
-            [1, 2],
-            3,
-            [0, 1, 2])
-        end.to raise_error(SystemExit)
-      end
-    end
-
-    context 'topic with no partiton' do
-      it 'should fail' do
-        expect do
-          assignments = reassign.generate_assignment_load_balanced(
-            {"topic_name" => FactoryGirl.build(:topic)},
-            [1, 2],
-            2)
-        end.to raise_error(SystemExit)
-
-        expect do
-          assignments = reassign.generate_assignment_smart(
-            {"topic_name" => FactoryGirl.build(:topic)},
-            [1, 2],
-            2,
-            [0, 1, 2])
-        end.to raise_error(SystemExit)
       end
     end
   end

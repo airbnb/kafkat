0b0974136b78be9aa6544902ba5fba387791a88a
Tong Wei <tong.wei@airbnb.com>
2016-11-17 10:37:30 -0800

    Format output with printf

--- lib/kafkat/command/verify-replicas.rb
+++ lib/kafkat/command/verify-replicas.rb
@@ -27,6 +27,7 @@ module Kafkat
 
         partition_replica_size, partition_replica_size_stat = verify_replicas(broker, topics)
 
+        print_summary = !print_details || print_summary
         print_mismatched_partitions(partition_replica_size, partition_replica_size_stat, print_details, print_summary)
       end
 
@@ -41,11 +42,9 @@ module Kafkat
           t.partitions.each do |p|
             replica_size = p.replicas.length
 
-            next if broker && !(p.replicas.include? broker)
+            next if broker && !p.replicas.include?(broker)
 
-            if !partition_replica_size_stat[t.name].key? replica_size
-              partition_replica_size_stat[t.name][replica_size] = 0
-            end
+            partition_replica_size_stat[t.name][replica_size] ||= 0
             partition_replica_size_stat[t.name][replica_size] += 1
 
             partition_replica_size[t.name][p.id] = replica_size
@@ -57,22 +56,23 @@ module Kafkat
       end
 
       def print_mismatched_partitions(partition_replica_size, partition_replica_size_stat, print_details, print_summary)
+        topic_column_width = partition_replica_size.keys.max_by(&:length).length
         if print_details
-          print "topic\tpartition\treplica_size\treplication_factor\n"
+          printf "%-#{topic_column_width}s %-10s %-15s %-20s\n", "topic", "partition", "replica_size", "replication_factor"
 
           partition_replica_size.each do |topic_name, partition|
             replication_factor = partition_replica_size_stat[topic_name].key(partition_replica_size_stat[topic_name].values.max)
 
             partition.each do |id, replica_size|
               if replica_size != replication_factor
-                print "#{topic_name}\t#{id}\t#{replica_size}\t#{replication_factor}\n"
+                printf "%-#{topic_column_width}s %-10d %-15d %-20d\n", topic_name, id, replica_size, replication_factor
               end
             end
           end
         end
 
-        if !print_details || print_summary
-          print "topic\treplica_size\tcount\tpercentage\treplication_factor\n"
+        if print_summary
+          printf "%-#{topic_column_width}s %-15s %-10s %-15s %-20s\n", "topic", "replica_size", "count", "percentage", "replication_factor"
           partition_replica_size_stat.each do |topic_name, partition|
             if partition.values.size > 1
               replication_factor = partition_replica_size_stat[topic_name].key(partition_replica_size_stat[topic_name].values.max)
@@ -80,7 +80,8 @@ module Kafkat
               partition.each { |key, value| num_partitions += value }
 
               partition.each do |replica_size, cnt|
-                print "#{topic_name}\t#{replica_size}\t#{cnt}\t#{(cnt * 100 / num_partitions).to_i}%\t#{replication_factor}\n"
+                printf "%-#{topic_column_width}s %-15d %-10d %-15d %-20d\n", topic_name, replica_size, cnt, (cnt * 100 /num_partitions)
+                                                                                           .to_i, replication_factor
               end
             end
           end
